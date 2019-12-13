4821fc7f00c4e814110d467af34f0b1d2b8790ee
赵义 <yi.zhao@51shaoxi.com>
2019-12-13 16:14:33 +0800

    更新

---
+++ src/main/java/com/mybaitsplus/devtools/core/ExcelConstant.java
@@ -0,0 +1,22 @@
+package com.mybaitsplus.devtools.core;
+
+public class ExcelConstant {
+
+
+    /**
+     * 每个sheet存储的记录数 100W
+     */
+    public static final Integer PER_SHEET_ROW_COUNT = 1000000;
+
+    /**
+     * 每次向EXCEL写入的记录数(查询每页数据大小) 20W
+     */
+    public static final Integer PER_WRITE_ROW_COUNT = 200000;
+
+
+    /**
+     * 每个sheet的写入次数 5
+     */
+    public static final Integer PER_SHEET_WRITE_COUNT = PER_SHEET_ROW_COUNT / PER_WRITE_ROW_COUNT;
+
+}
